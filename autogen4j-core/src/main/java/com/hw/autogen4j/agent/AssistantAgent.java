44d317688b2904f99487825c9c1fc565e983ee86
favalot <156077246+favalot@users.noreply.github.com>
2022-02-14 21:43:36 +0000

    removed extra space

--- autogen4j-core/src/main/java/com/hw/autogen4j/agent/AssistantAgent.java
+++ autogen4j-core/src/main/java/com/hw/autogen4j/agent/AssistantAgent.java
@@ -40,4 +40,10 @@ public class AssistantAgent extends ConversableAgent {
                     If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
                     When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
                     Reply "TERMINATE" in the end when everything is done.
-         
+                    """;
+
+    private AssistantAgent(Builder builder) {
+        super(builder);
+    }
+
+    p

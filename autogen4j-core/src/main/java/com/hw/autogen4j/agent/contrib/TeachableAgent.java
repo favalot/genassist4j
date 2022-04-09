ccdf0f747e741881c8e9de8403a91eba6d55bc11
favalot <156077246+favalot@users.noreply.github.com>
2022-04-09 06:15:52 +0000

    l10n: Updated Bulgarian (bg) translation to 100%

--- autogen4j-core/src/main/java/com/hw/autogen4j/agent/contrib/TeachableAgent.java
+++ autogen4j-core/src/main/java/com/hw/autogen4j/agent/contrib/TeachableAgent.java
@@ -21,4 +21,6 @@ package com.hw.autogen4j.agent.contrib;
 import com.hw.autogen4j.agent.ConversableAgent;
 
 /**
- * Teachable Age
+ * Teachable Agent, a subclass of ConversableAgent using a vector database to remember user teachings.
+ *
+ * @autho

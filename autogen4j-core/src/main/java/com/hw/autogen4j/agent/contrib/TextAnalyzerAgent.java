17659ddca18a5c701a1137e471ec0ca20c62cace
favalot <156077246+favalot@users.noreply.github.com>
2022-05-11 12:34:00 +0000

    Added test code

--- autogen4j-core/src/main/java/com/hw/autogen4j/agent/contrib/TextAnalyzerAgent.java
+++ autogen4j-core/src/main/java/com/hw/autogen4j/agent/contrib/TextAnalyzerAgent.java
@@ -21,4 +21,8 @@ package com.hw.autogen4j.agent.contrib;
 import com.hw.autogen4j.agent.ConversableAgent;
 
 /**
- * Text Analysis agent, a subclass of ConversableAgent designed to analyze text as ins
+ * Text Analysis agent, a subclass of ConversableAgent designed to analyze text as instructed.
+ *
+ * @author HamaWhite
+ */
+public class TextAnalyzerAgent extends ConversableAgent {

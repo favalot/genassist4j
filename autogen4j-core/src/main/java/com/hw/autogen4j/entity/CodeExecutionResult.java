3a0d922470a684625dfb330c31b9177702791c3d
favalot <156077246+favalot@users.noreply.github.com>
2022-08-02 20:17:24 +0000

    code tidying

--- autogen4j-core/src/main/java/com/hw/autogen4j/entity/CodeExecutionResult.java
+++ autogen4j-core/src/main/java/com/hw/autogen4j/entity/CodeExecutionResult.java
@@ -13,4 +13,11 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the 
+ * limitations under the License.
+ */
+
+package com.hw.autogen4j.entity;
+
+
+/**
+ * Represents the result of code exec

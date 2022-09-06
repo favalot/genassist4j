3bb9fecd9512f656778441be405bbeedc8080ae4
favalot <156077246+favalot@users.noreply.github.com>
2022-09-06 06:54:15 +0000

    added npm badge

--- autogen4j-core/src/main/java/com/hw/autogen4j/exception/Autogen4jException.java
+++ autogen4j-core/src/main/java/com/hw/autogen4j/exception/Autogen4jException.java
@@ -1,2 +1,4 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distribut

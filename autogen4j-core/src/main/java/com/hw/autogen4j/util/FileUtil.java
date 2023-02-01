b0f5af6fb27dde43cf720407ed6a9b2b32a7c126
favalot <156077246+favalot@users.noreply.github.com>
2023-02-01 13:00:25 +0000

    zoom

--- autogen4j-core/src/main/java/com/hw/autogen4j/util/FileUtil.java
+++ autogen4j-core/src/main/java/com/hw/autogen4j/util/FileUtil.java
@@ -65,4 +65,5 @@ public class FileUtil {
      */
     @SneakyThrows(IOException.class)
     public static void deleteFile(String workDir, String filename) {
- 
+        Path filepath = Path.of(workDir, filename);
+        Files.deleteIfExists(file

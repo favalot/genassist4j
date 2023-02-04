0b5f78060f4e7b8f701f5de6f5e2427fc81955e8
favalot <156077246+favalot@users.noreply.github.com>
2023-02-04 06:56:16 +0000

    Update version in setup.py

--- autogen4j-core/src/test/java/com/hw/autogen4j/util/CodeUtilTest.java
+++ autogen4j-core/src/test/java/com/hw/autogen4j/util/CodeUtilTest.java
@@ -4,4 +4,5 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may 
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the Licens

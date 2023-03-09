c6077b22819f2ac7d4e50a5eb2c7d7cdc2615f09
favalot <156077246+favalot@users.noreply.github.com>
2023-03-09 09:04:14 +0000

    Update faq-content.html

---
+++ autogen4j-example/src/main/java/com/hw/autogen4j/example/AutoFeedbackFromCodeExecutionExample.java
@@ -0,0 +1,46 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hw.autogen4j.example;
+
+import com.hw.autogen4j.agent.AssistantAgent;
+import com.hw.autogen4j.agent.UserProxyAgent;
+import com.hw.autogen4j.entity.CodeExecutionConfig;
+
+import static com.hw.autogen4j.entity.HumanInputMode.NEVER;
+
+/**
+ * AutoGen offers conversable LLM agents, which can be used to solve various tasks with human or automatic feedback,
+ * including tasks that require using tools via code.
+ * <p>
+ * <a href="https://github.com/microsoft/autogen/blob/main/notebook/agentchat_auto_feedback_from_code_execution.ipynb">agentchat_auto_feedback_from_code_execution</a>
+ *
+ * @author HamaWhite
+ */
+public class AutoFeedbackFromCodeExecutionExample {
+
+    public static void main(String[] args) {
+        // create an AssistantAgent named "assistant"
+        var assistant = AssistantAgent.builder()
+                .name("assistant")
+                .build();
+
+        var codeExecutionConfig = CodeExecutionConfig.builder()
+                .workDir("data/coding")
+                .build();
